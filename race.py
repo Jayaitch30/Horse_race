2d9fe9ae72a57646a923bf9aa77a490ae1af509f
Jayaitch30 <84965841+Jayaitch30@users.noreply.github.com>
2021-07-02 13:05:22 +0300

    Add files via upload

--- race.py
+++ race.py
@@ -7,6 +7,14 @@ import os
 racecourse = turtle.Screen()
 start = turtle.Turtle()
 winner = turtle.Turtle()
+moneyman = turtle.Turtle()
+current_money = turtle.Turtle()
+kincsem_bet = 0
+BlackCaviar_bet = 0
+PeppersPride_bet = 0
+Eclipse_bet = 0
+coins = 20
+gambler = ''
 class horses(Turtle):
     def __init__(self, name, move, position):
         super().__init__(shape=os.path.join('resources', 'horse.gif'), visible=True)
@@ -31,40 +39,196 @@ def init():
     racecourse.title("Horse race")
     racecourse.onscreenclick(buttonclick, 1)
     racecourse.listen()
+    current_money.speed('fastest')
+    moneyman.speed('fastest')
+    moneyman.penup()
+    current_money.penup()
+    moneyman.hideturtle()
+    current_money.hideturtle()
+    current_money.goto(-50, 250)
 
 def buttonclick(x, y):
     if start.isvisible():
         if x > -89 and x < 89 and y > -44 and y < 44:
             start.hideturtle()
-            game()
+            bets()
+class bets_function():
+    def __init__(self, pos):
+        self.pos = pos
+        global gambler
+        if self.pos[0] > -15 and self.pos[0] < 100 and self.pos[1] > 165 and self.pos[1] < 210:
+            gambler = 'kincsem'
+            place_bet()
+        elif self.pos[0] > -15 and self.pos[0] < 100 and self.pos[1] > 13 and self.pos[1] < 55:
+            gambler = "BlackCaviar"
+            place_bet()
+        elif self.pos[0] > -15 and self.pos[0] < 100 and self.pos[1] > -140 and self.pos[1] < -95:
+            gambler = "PeppersPride"
+            place_bet()
+        elif self.pos[0] > -15 and self.pos[0] < 100 and self.pos[1] > -285 and self.pos[1] < -240:
+            gambler = "Eclipse"
+            place_bet()
+        if self.pos[0] > -80 and self.pos[0] < -30 and self.pos[1] > 165 and self.pos[1] < 210:
+            kincsem_add_bet()
+        elif self.pos[0] > -80 and self.pos[0] < -30 and self.pos[1] > 10 and self.pos[1] < 55:
+            BlackCaviar_add_bet()
+        elif self.pos[0] > -80 and self.pos[0] < -30 and self.pos[1] > -135 and self.pos[1] < -100:
+            PeppersPride_add_bet()
+        elif self.pos[0] > -80 and self.pos[0] < -30 and self.pos[1] > -280 and self.pos[1] < -240:
+            Eclipse_add_bet()
+def place_bet():
+    get_ready()
+    game()
+def get_ready():
+    racecourse.bgpic(os.path.join('resources', 'race_track.gif'))
+    moneyman.clear()
+    global kincsem_bet
+    global BlackCaviar_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    global coins
+    global gambler
+    if gambler == 'kincsem':
+        coins -= kincsem_bet
+    if gambler == "BlackCaviar":
+        coins -= BlackCaviar_bet
+    if gambler == "PeppersPride":
+        coins -= PeppersPride_bet
+    if gambler == "Eclipse":
+        coins -= Eclipse_bet
 
+def kincsem_add_bet():
+    global kincsem_bet
+    global coins
+    global BlackCaviar_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    BlackCaviar_bet = 0
+    PeppersPride_bet = 0
+    Eclipse_bet = 0
+    if kincsem_bet < coins:
+        kincsem_bet += 1
+        moneyman.clear()
+        moneyman.goto(-145, 165)
+        moneyman.write(kincsem_bet, align="center", font=("Comic Sans MS", 24, "normal"))
+def BlackCaviar_add_bet():
+    global BlackCaviar_bet
+    global kincsem_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    kincsem_bet = 0
+    PeppersPride_bet = 0
+    Eclipse_bet = 0
+    global coins
+    if BlackCaviar_bet < coins:
+        BlackCaviar_bet += 1
+        moneyman.clear()
+        moneyman.goto(-143, 10)
+        moneyman.write(BlackCaviar_bet, align="center", font=("Comic Sans MS", 24, "normal"))
+def PeppersPride_add_bet():
+    global BlackCaviar_bet
+    global kincsem_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    kincsem_bet = 0
+    BlackCaviar_bet = 0
+    Eclipse_bet = 0
+    global coins
+    if PeppersPride_bet < coins:
+        PeppersPride_bet += 1
+        moneyman.clear()
+        moneyman.goto(-143, -140)
+        moneyman.write(PeppersPride_bet, align="center", font=("Comic Sans MS", 24, "normal"))
+def Eclipse_add_bet():
+    global BlackCaviar_bet
+    global kincsem_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    kincsem_bet = 0
+    BlackCaviar_bet = 0
+    PeppersPride_bet = 0
+    global coins
+    if Eclipse_bet < coins:
+        Eclipse_bet += 1
+        moneyman.clear()
+        moneyman.goto(-143, -290)
+        moneyman.write(Eclipse_bet, align="center", font=("Comic Sans MS", 24, "normal"))
+def bets():
+    global coins
+    global kincsem_bet
+    global BlackCaviar_bet
+    global PeppersPride_bet
+    global Eclipse_bet
+    kincsem_bet = 0
+    BlackCaviar_bet = 0
+    PeppersPride_bet = 0
+    Eclipse_bet = 0
+    racecourse.bgpic(os.path.join('resources', 'Bet_screen.gif'))
+    current_money.clear()
+    if racecourse.bgpic() == os.path.join('resources', 'Bet_screen.gif'):
+        current_money.write("Coins: {}".format(coins), align="center",font=("Comic Sans MS", 24, "normal"))
+    racecourse.onscreenclick(findpos)
+    racecourse.listen()
+def findpos(x, y):
+    position = [x, y]
+    bets_function(position)
 def game():
     Kincsem = horses("Kincsem", 0, (-470, 230))
     BlackCaviar = horses("BlackCaviar", 0, (-470, 90))
     PeppersPride = horses("Peppers Pride", 0, (-470, -60))
     Eclipse = horses("Eclipse", 0, (-470, -220))
+    Kincsem.showturtle()
+    BlackCaviar.showturtle()
+    PeppersPride.showturtle()
+    Eclipse.showturtle()
+    current_money.clear()
     while running:
-        Kincsem.motion()
-        if Kincsem.xcor() >= 450:
-            winner.write("Kincsem is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
-            time.sleep(3)
-            restart()
-        BlackCaviar.motion()
-        if BlackCaviar.xcor() >= 450:
-            winner.write("Black Caviar is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
-            time.sleep(3)
-            restart()
-        PeppersPride.motion()
-        if PeppersPride.xcor() >= 450:
-            winner.write("Peppers Pride is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
-            time.sleep(3)
-            restart()
-        Eclipse.motion()
-        if Eclipse.xcor() >= 450:
-            winner.write("Eclipse is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
-            time.sleep(3)
-            restart()
+        global coins
+        if racecourse.bgpic() == os.path.join('resources', 'race_track.gif'):
+            Kincsem.motion()
+            if Kincsem.xcor() >= 450:
+                winner.write("Kincsem is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
+                time.sleep(3)
+                coins += kincsem_bet*2
+                restart()
+                bets()
+        else:
+            return bets()
+        if racecourse.bgpic() == os.path.join('resources', 'race_track.gif'):
+            BlackCaviar.motion()
+            if BlackCaviar.xcor() >= 450:
+                winner.write("Black Caviar is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
+                coins += BlackCaviar_bet * 2
+                time.sleep(3)
+                restart()
+                bets()
+        else:
+            return bets()
+        if racecourse.bgpic() == os.path.join('resources', 'race_track.gif'):
+            PeppersPride.motion()
+            if PeppersPride.xcor() >= 450:
+                winner.write("Peppers Pride is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
+                coins += PeppersPride_bet * 2
+                time.sleep(3)
+                restart()
+                bets()
+        else:
+            return bets()
+        if racecourse.bgpic() == os.path.join('resources', 'race_track.gif'):
+            Eclipse.motion()
+            if Eclipse.xcor() >= 450:
+                winner.write("Eclipse is the winner!", align="center",font=("Comic Sans MS", 24, "normal"))
+                coins += Eclipse_bet * 2
+                time.sleep(3)
+                restart()
+                bets()
+        else:
+            return bets()
         def restart():
+            Kincsem.hideturtle()
+            BlackCaviar.hideturtle()
+            PeppersPride.hideturtle()
+            Eclipse.hideturtle()
             Kincsem.setposition(-470, 230)
             BlackCaviar.setposition(-470, 90)
             PeppersPride.setposition(-470, -60)
@@ -72,10 +236,11 @@ def game():
             winner.clear()
 
 
+
     racecourse.bye()
 
 def kESC():
-    if start.isvisible():
+    if start.isvisible() or racecourse.bgpic() == os.path.join('resources', 'Bet_screen.gif'):
         racecourse.bye()
     else:
         global running
